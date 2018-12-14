00b89b6317dc411ae5347ea445292fcb46217195
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-12-14 12:42:25 +0100

    wip

--- backend/tracim_backend/lib/calendar/determiner.py
+++ backend/tracim_backend/lib/calendar/determiner.py
@@ -8,28 +8,32 @@ if typing.TYPE_CHECKING:
     from tracim_backend import TracimRequest
 
 
+CALDAV_READ_METHODS = (
+    'GET',
+    'HEAD',
+    'OPTIONS',
+    'PROPFIND',
+    'LOCK',
+    'UNLOCK',
+    'REPORT',
+)
+CALDAV_WRITE_METHODS = (
+    'PUT',
+    'DELETE',
+    'POST',
+    'PROPPATCH',
+    'COPY',
+    'MOVE',
+    'MKCOL',
+    'MKCALENDAR',
+)
+
+
 class CaldavAuthorizationDeterminer(object):
     def determine_requested_mode(self, request: 'TracimRequest') -> DavAuthorization:
-        if request.method in (
-            'GET',
-            'HEAD',
-            'OPTIONS',
-            'PROPFIND',
-            'LOCK',
-            'UNLOCK',
-            'REPORT',
-        ):
+        if request.method in CALDAV_READ_METHODS:
             return DavAuthorization.READ
-        elif request.method in (
-            'PUT',
-            'DELETE',
-            'POST',
-            'PROPPATCH',
-            'COPY',
-            'MOVE',
-            'MKCOL',
-            'MKCALENDAR',
-        ):
+        elif request.method in CALDAV_WRITE_METHODS:
             return DavAuthorization.WRITE
         else:
             logger.warning(
